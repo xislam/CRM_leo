17347836559e40e51c6f6fb79cbdfc24a434d81e
Islam <50731444+xislam@users.noreply.github.com>
2023-08-19 21:33:37 +0600

    db pslq

--- root/settings.py
+++ root/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bgw5xs9677mf-ckhzyife2ntbofffj9*e#fp9u_d3*9pekm+fc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '194.67.86.225']
 
 # Application definition
 
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
